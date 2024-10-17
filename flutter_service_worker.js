6d3cfa7af8aa9776d852226fda92d03ef659181d
Leo <sixpathssenin@gmail.com>
2024-10-18 00:07:43 +0530

    Fixing flutter bootstrap.js missing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "3ab6d18e52dfb4a6ad38c25a89b53e5b",
+"flutter_bootstrap.js": "3fe86a87897c02d2ef5511a1918b3082",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
